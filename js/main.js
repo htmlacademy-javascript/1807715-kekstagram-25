ad48ea5c2619b95b248a6f3a84bb35db6c52e983
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2022-02-22 16:51:59 +0300

    Внесет правки в main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-function getRandomNumber(min,max) {
+function getRandomNumber(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   if (min >= max) {
@@ -8,7 +8,7 @@ function getRandomNumber(min,max) {
 }
 getRandomNumber();
 
-function getStringLength(comment,maxLength) {
+function getStringLength(comment, maxLength) {
   if (comment.length <= maxLength) {
     return true;
   }
