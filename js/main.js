710f63216ebc4937e8d4a70715046c79f6961071
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2022-03-06 03:01:57 +0300

    Добавит main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,33 @@
+function getStringLength(comment, maxLength) {
+  if (comment.length <= maxLength) {
+    return true;
+  }
+}
+getStringLength();
+
+const NAMES = [
+  'Tom',
+  'George',
+  'Carl',
+  'Natali',
+  'Alan',
+  'John',
+  'Bob',
+  'Randy',
+  'Margo',
+];
+
+const MESSAGES = [
+  'Всё отлично!',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+];
+
+const SIMILAR_PHOTO_DESCRIPTION = 25;
+const MAXIMUM_ID = 25;
+const MAXIMUM_URL = 25;
+const MAXIMUM_LIKES = 200;
+const MAXIMUM_AVATAR = 6;
+
 function getRandomNumber(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
@@ -6,11 +36,31 @@ function getRandomNumber(min, max) {
   }
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-getRandomNumber();
 
-function getStringLength(comment, maxLength) {
-  if (comment.length <= maxLength) {
-    return true;
-  }
-}
-getStringLength();
+const getRandomArrayElement = (elements) => elements[getRandomNumber(0, elements.length - 1)];
+
+const createPhotoDescription = () => ({
+  id: getRandomNumber(1, MAXIMUM_ID),
+  url: `photos/${getRandomNumber(1, MAXIMUM_URL)}.jpg`,
+  description: 'Вкусный завтрак у моря.',
+  likes: getRandomNumber(15, MAXIMUM_LIKES),
+  comments: [
+    {
+      id: getRandomNumber(),
+      avatar: `img/avatar-${getRandomNumber(1, MAXIMUM_AVATAR)}.svg`,
+      name: getRandomArrayElement(NAMES),
+      message: getRandomArrayElement(MESSAGES),
+    },
+    {
+      id: getRandomNumber(),
+      avatar: `img/avatar-${getRandomNumber(1, MAXIMUM_AVATAR)}.svg`,
+      name: getRandomArrayElement(NAMES),
+      message: getRandomArrayElement(MESSAGES),
+    }
+  ]
+});
+
+const similarPhotoDescription = Array.from({length: SIMILAR_PHOTO_DESCRIPTION}, createPhotoDescription);
+
+createPhotoDescription();
+
