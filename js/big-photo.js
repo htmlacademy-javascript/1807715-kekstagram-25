bf562d4386d20c1242bc5b43c12b36f51ed7f4a3
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2022-03-22 17:27:33 +0300

    Добавит изменения в updateBigPicture

--- js/big-photo.js
+++ js/big-photo.js
@@ -5,7 +5,6 @@ const popup = document.querySelector('.big-picture');
 const openPopup = document.querySelector('.pictures');
 const closePopup = popup.querySelector('.big-picture__cancel');
 
-
 closePopup.addEventListener('click', () => {
   popup.classList.add('hidden');
 });
