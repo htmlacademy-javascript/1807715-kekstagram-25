31a78b28bb64da4e2a29c4a3af3b7b31366c2258
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2022-03-23 14:17:33 +0300

    Добавит изменения в main.js

--- js/big-photo.js
+++ js/big-photo.js
@@ -31,24 +31,4 @@ const updateBigPicture = (clickedElement) => {
   addComments(clickedElement);
 };
 
-openPopup.addEventListener('click', (evt) => {
-  if(evt.target.parentNode.matches('.picture')){
-    const getObject = evt.target.getAttribute('src');
-    const dataObject = similarPhotoDescription.find((o) => o.url === getObject);
-    updateBigPicture(dataObject);
-    document.body.classList.add('modal-open');
-    popup.classList.remove('hidden');
-    popup.querySelector('.social__comment-count').classList.add('hidden');
-    popup.querySelector('.comments-loader').classList.add('hidden');
-    closePopup.addEventListener('click', () => {
-      document.body.classList.remove('modal-open');
-      popup.classList.add('hidden');
-    });
-  }
-});
-
-document.addEventListener('keydown', (evt) => {
-  if(evt.code === 'Escape') {
-    popup.classList.add('hidden');
-  }
-});
+export {popup, openPopup, closePopup, updateBigPicture, similarPhotoDescription};
--- js/main.js
+++ js/main.js
@@ -1,2 +1,25 @@
 import './photo.js';
-import './big-photo.js';
+import {popup, openPopup, closePopup, updateBigPicture, similarPhotoDescription} from './big-photo.js';
+
+openPopup.addEventListener('click', (evt) => {
+  if(evt.target.parentNode.matches('.picture')){
+    const getObject = evt.target.getAttribute('src');
+    const dataObject = similarPhotoDescription.find((o) => o.url === getObject);
+    updateBigPicture(dataObject);
+    document.body.classList.add('modal-open');
+    popup.classList.remove('hidden');
+    popup.querySelector('.social__comment-count').classList.add('hidden');
+    popup.querySelector('.comments-loader').classList.add('hidden');
+    closePopup.addEventListener('click', () => {
+      document.body.classList.remove('modal-open');
+      popup.classList.add('hidden');
+    });
+  }
+});
+
+document.addEventListener('keydown', (evt) => {
+  if(evt.code === 'Escape') {
+    popup.classList.add('hidden');
+  }
+});
+
