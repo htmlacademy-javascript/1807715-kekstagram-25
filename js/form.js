fca00ff2726cdca0668bc20d9049a9037d292be5
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2022-03-30 02:37:13 +0300

    Добавит валидацию  hashtags

--- js/form.js
+++ js/form.js
@@ -1,14 +1,28 @@
 const form = document.querySelector('.img-upload__form');
-
+const hashtags = form.querySelector('.text__hashtags');
 const pristine = new Pristine(form);
+
+function validateHashtag(hashtags) {
+  const hashtagFormat = /^#[A-Za-zA-Яa-яЁё0-9]{1,19}$\s/;
+  if(hashtags.value.match(hashtagFormat))
+  {
+    return true;
+  }
+  hashtags.focus();
+  return ('Невалидный формат');
+}
+
+pristine.addValidator(form.querySelector('.text__hashtags'), validateHashtag);
+
+function validateComments (value) {
+  return value.length <= 140;
+}
+pristine.addValidator(form.querySelector('.social__footer-text'),
+  validateComments,
+  'Максимальная длина комментария 140 символов',
+);
 
 form.addEventListener('submit', (evt) => {
   evt.preventDefault();
-
-  const isValid = pristine.validate();
-  if (isValid) {
-    console.log('Можно отправлять');
-  } else {
-    console.log('Форма невалидна');
-  }
+  pristine.validate();
 });
