b55b68565df73e586e691afc5754187682bfe14f
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2022-04-01 03:14:08 +0300

    Добавит валидацию

--- js/form.js
+++ js/form.js
@@ -1,8 +1,12 @@
 import {isEscapeKey} from './util.js';
 
+const form = document.querySelector('.img-upload__form');
 const uploadPhotoForm = document.querySelector('#upload-file');
 const openForm = document.querySelector('.img-upload__overlay');
 const closeForm = document.querySelector('#upload-cancel');
+const hashtags = uploadPhotoForm.querySelector('.text__hashtags');
+
+const pristine = new Pristine(form);
 
 const onFormKeydown = (evt) => {
   if (isEscapeKey(evt)) {
@@ -15,6 +19,24 @@ function openFormModal () {
   document.body.classList.add('modal-open');
   openForm.classList.remove('hidden');
   document.addEventListener('keydown', onFormKeydown);
+}
+
+function validateHashtag(hashtags) {
+  const hashtagFormat = /^#[A-Za-zA-Яa-яЁё0-9]{1,19}$\s/;
+  if(hashtags.value.match(hashtagFormat))
+  {
+    return true;
+  }
+  hashtags.focus();
+  return ('Невалидный формат');
+}
+
+function validateComments(value) {
+  if(value.length <= 140)
+  {
+    return true;
+  }
+  return ('Максимальная длина комментария 140 символов');
 }
 
 function closeFormModal () {
@@ -30,3 +52,15 @@ uploadPhotoForm.addEventListener('change', () => {
 closeForm.addEventListener('click', () => {
   closeFormModal();
 });
+
+uploadPhotoForm.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+  pristine.validate();
+
+  const isValid = pristine.validate();
+  if (isValid) {
+    return true;
+  }
+  return('Форма невалидна');
+});
+
