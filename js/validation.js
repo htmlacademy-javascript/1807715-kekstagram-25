273e3c2a0bb0d8909d387b16069bef0bb82da1bd
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2022-04-06 17:51:50 +0300

    Внесет изменения в validation.js

--- js/validation.js
+++ js/validation.js
@@ -2,8 +2,8 @@ import {uploadPhotoForm} from './form.js';
 import {isEscapeKey} from './util.js';
 
 const form = document.querySelector('.img-upload__form');
-const hashtags = form.querySelector('.text__hashtags');
-const commentsField = form.querySelector('.text__description');
+const hashtags = document.querySelector('.text__hashtags');
+const commentsField = document.querySelector('.text__description');
 const MAX_HASHTAGS = 5;
 const MIN_LENGTH = 2;
 const MAX_LENGTH = 20;
@@ -12,8 +12,10 @@ const MAX_COMMENT_SYMBOLS = 140;
 const pristine = new Pristine(form, {
   classTo: 'img-upload__text',
   errorTextParent: 'img-upload__text',
-  errorTextClass: 'error-text',
+  errorTextClass: 'form__error',
 }, false);
+
+const checkHashtag = (hashtag, array) => (hashtag[0] === '#') && (hashtag.length > MIN_LENGTH) && (hashtag.length <= MAX_LENGTH) && (hashtag.lastIndexOf('#') === 0) && (array.indexOf(hashtag) === array.lastIndexOf(hashtag));
 
 function validateHashtag() {
   let hashtagsValues = hashtags.value.toLowerCase();
@@ -21,14 +23,18 @@ function validateHashtag() {
   if (hashtagsValues.length > MAX_HASHTAGS) {
     return false;
   }
-  const a = hashtagsValues.filter((element) => checkHashtag(element, hashtagsValues));
-  if (a.length !== hashtagsValues.length) {
+  const elementValue = hashtagsValues.filter((element) => checkHashtag(element, hashtagsValues));
+  if (elementValue.length !== hashtagsValues.length) {
     return false;
   }
   return true;
 }
 
-const checkHashtag = (hashtag, array) => (hashtag[0] === '#') && (hashtag.length > MIN_LENGTH) && (hashtag.length <= MAX_LENGTH) && (hashtag.lastIndexOf('#') === 0) && (array.indexOf(hashtag) === array.lastIndexOf(hashtag));
+pristine.addValidator(
+  hashtags,
+  validateHashtag,
+  'Количество хештегов должно быть не более 5',
+);
 
 function validateComments() {
   commentsField.addEventListener('input', () => {
@@ -38,12 +44,6 @@ function validateComments() {
     }
   });
 }
-
-pristine.addValidator(
-  uploadPhotoForm,
-  validateHashtag,
-  'Количество хештегов должно быть не более 5',
-);
 
 pristine.addValidator(
   uploadPhotoForm,
