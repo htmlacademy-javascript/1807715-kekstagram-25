df813185a55de152d6641c4101748f6657b4eed4
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2022-04-16 20:21:13 +0300

    Добавит реализацию эффектов

--- js/effects.js
+++ js/effects.js
@@ -1,21 +1,18 @@
-//1.По умолчанию должен быть выбран эффект «Оригинал».
-//2.На изображение может накладываться только один эффект.
-//3.При смене эффекта, выбором одного из значений среди радиокнопок .effects__radio, добавить картинке внутри .img-upload__preview CSS-класс,
-//соответствующий эффекту. Например, если выбран эффект .effect-chrome,
-//изображению нужно добавить класс effects__preview--chrome.
-//4.Интенсивность эффекта регулируется перемещением ползунка в слайдере.
-const imgUpload = document.querySelector('.img-upload__preview');
+const imgUpload = document.querySelector('.img-upload__preview img');
 const effectsValue = document.querySelector('.effect-level__value');
 const slider = document.querySelector('.effect-level__slider');
-const effectNone = document.querySelector('#effect-none');
-const effectChrome = document.querySelector('#effect-chrome');
-const effectSepia = document.querySelector('#effect-sepia');
-const effectMarvin = document.querySelector('#effect-marvin');
-const effectPhobos = document.querySelector('#effect-phobos');
-const effectHeat = document.querySelector('#effect-heat');
+const effectNone = document.querySelector('#effect-none').value;
+const effectChrome = document.querySelector('#effect-chrome').value;
+const effectSepia = document.querySelector('#effect-sepia').value;
+const effectMarvin = document.querySelector('#effect-marvin').value;
+const effectPhobos = document.querySelector('#effect-phobos').value;
+const effectHeat = document.querySelector('#effect-heat').value;
+const effectInputs = document.querySelectorAll('.effects__radio');
 
 effectsValue.value = 100;
-let currentEffect = 'none';
+let currentEffectType = effectNone;
+
+// создает slider
 
 noUiSlider.create(slider, {
   range: {
@@ -39,60 +36,110 @@ noUiSlider.create(slider, {
 });
 
 //добавит картинке .img-upload__preview CSS-класс, соответствующий эффекту
+
 const addEffects = (value) => {
-  imgUpload.querySelector('img').className = '';
-  if (value !== 'none') {
-    imgUpload.querySelector('img').classList.add(`effects__preview--${value}`);
+  imgUpload.className = '';
+  if (value !== effectNone) {
+    imgUpload.classList.add(`effects__preview--${value}`);
   }
 };
 
-addEffects(currentEffect);
+addEffects(effectNone);
 
 //измененит уровень интенсивности эффекта
+
 const changeEffectLevel = (type, value) => {
-  if (type === 'none') {
-    imgUpload.querySelector('img').style.filter = 'none';
-  } else if (type === 'gracescale') {
-    imgUpload.querySelector('img').style.filter = `${type}(${value})`;
-  } else if (type === 'sepia') {
-    imgUpload.querySelector('img').style.filter = `${type}(${value})`;
-  } else if (type === 'invert') {
-    imgUpload.querySelector('img').style.filter = `${type}(${value}%)`;
-  } else if (type === 'blur') {
-    imgUpload.querySelector('img').style.filter = `${type}(${value}px)`;
-  } else if (type ==='brightness') {
-    imgUpload.querySelector('img').style.filter = `${type}(${value})`;
+  if (type === effectNone) {
+    imgUpload.style.filter = 'none';
+  } else if (type === effectMarvin) {
+    imgUpload.style.filter = `invert(${value}%)`;
+  } else if (type === effectPhobos) {
+    imgUpload.style.filter = `blur(${value}px)`;
+  } else if (type === effectChrome) {
+    imgUpload.style.filter = `grayscale(${value})`;
+  } else if (type === effectSepia) {
+    imgUpload.style.filter = `sepia(${value})`;
+  } else if (type === effectHeat) {
+    imgUpload.style.filter = `brightness(${value})`;
   }
 };
 
 slider.noUiSlider.on('update', () => {
   effectsValue.value = slider.noUiSlider.get();
+  changeEffectLevel(currentEffectType, effectsValue.value);
 });
 
-effectChrome.addEventListener('change', (evt) => {
-
-  if (evt.target.checked) {
-    slider.noUiSlider.updateOptions({
-      range: {
-        min: 0,
-        max: 1,
-      },
-      step: 0.1,
-    });
-  }
-});
+for(let i = 0; i < effectInputs.length; i++) {
+  effectInputs[i].addEventListener('change', (evt) => {
+    if (evt.target.value === effectChrome) {
+      currentEffectType = effectChrome;
+      slider.noUiSlider.updateOptions({
+        range: {
+          min: 0,
+          max: 1,
+        },
+        start: 1,
+        step: 0.1,
+      });
+      addEffects(effectChrome);
+    } else if (evt.target.value === effectMarvin) {
+      currentEffectType = effectMarvin;
+      slider.noUiSlider.updateOptions({
+        range: {
+          min: 0,
+          max: 100,
+        },
+        start: 100,
+        step: 1,
+      });
+      addEffects(effectMarvin);
+    } else if (evt.target.value === effectPhobos) {
+      currentEffectType = effectPhobos;
+      slider.noUiSlider.updateOptions({
+        range: {
+          min: 0,
+          max: 3,
+        },
+        start: 3,
+        step: 1,
+      });
+      addEffects(effectPhobos);
+    } else if (evt.target.value === effectSepia) {
+      currentEffectType = effectSepia;
+      slider.noUiSlider.updateOptions({
+        range: {
+          min: 0,
+          max: 1,
+        },
+        start: 1,
+        step: 0.1,
+      });
+      addEffects(effectSepia);
+    } else if (evt.target.value === effectHeat) {
+      currentEffectType = effectHeat;
+      slider.noUiSlider.updateOptions({
+        range: {
+          min: 0,
+          max: 3,
+        },
+        start: 3,
+        step: 0.1,
+      });
+      addEffects(effectHeat);
+    } else if (evt.target.value === effectNone) {
+      currentEffectType = effectNone;
+      slider.noUiSlider.updateOptions({
+        range: {
+          min: 0,
+          max: 100,
+        },
+        start: 100,
+        step: 1,
+      });
+      addEffects(effectNone);
+    }
+  });
+}
 
 //slider.noUiSlider.destroy();
 
-//6.При выборе эффекта «Оригинал» слайдер скрывается.
-//5.Уровень эффекта записывается в поле .effect-level__value. При изменении уровня интенсивности эффекта (предоставляется API слайдера),
-//CSS-стили картинки внутри .img-upload__preview обновляются следующим образом:
-/*Для эффекта «Хром» — filter: grayscale(0..1) с шагом 0.1;
-Для эффекта «Сепия» — filter: sepia(0..1) с шагом 0.1;
-Для эффекта «Марвин» — filter: invert(0..100%) с шагом 1%;
-Для эффекта «Фобос» — filter: blur(0..3px) с шагом 0.1px;
-Для эффекта «Зной» — filter: brightness(1..3) с шагом 0.1;
-Для эффекта «Оригинал» CSS-стили filter удаляются.*/
-
-//7.При переключении эффектов, уровень насыщенности сбрасывается до начального значения (100%):
-//слайдер, CSS-стиль изображения и значение поля должны обновляться
